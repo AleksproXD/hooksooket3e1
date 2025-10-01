1f9842030cec07027214103b00fdfc1e7676355f
alexbylegend03 <alexbylegend03@gmail.com>
2025-10-01 19:31:01 +0000

    🔥Creacion de babel y metro -comienzo de la pagina -index en 0

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, Pressable, StyleSheet } from "react-native";
+import "./global.css"
 
 export default function Index() {
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+
     </View>
   );
 }
+
